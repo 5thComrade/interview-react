1c2545d3e267e7b4ff81f23d0c2500740b8c9964
Antony Chiramel <antonychiramel07@gmail.com>
2023-04-27 12:34:44 +0000

    nitial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+// API URL: https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json
+
+function App() {
+  return (
+    <main>
+      <div>
+        <p>Show Count Here</p>
+        <button>Add 1</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
